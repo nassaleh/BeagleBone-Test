c2cb6e0d3af6ba196061bc741692adf732f55103
nassaleh <nick.assaleh@gmail.com>
2021-10-28 16:17:47 -0400

    Work in progress

---
+++ HighResTimers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HighResTimers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            Console.WriteLine($"Frequency: {Stopwatch.Frequency}");
+            Console.WriteLine($"IsHighResolution: {Stopwatch.IsHighResolution}");
+        }
+    }
+}
--- Website-Kestrel/Program.cs
+++ Website-Kestrel/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Security.Cryptography;
 using System.Threading;
 using BeagleBone;
+using Encryption_Demo;
 using Web;
 
 namespace BBB
@@ -10,6 +12,8 @@ namespace BBB
         static void Main(String[] args)
         {
             Console.WriteLine("Entering Main");
+
+            EncryptionTest();
 
             var exitEvent = new ManualResetEvent(false);
 
@@ -29,6 +33,34 @@ namespace BBB
             using var webWorker = new wsWorker();
             Thread webThread = new Thread(webWorker.DoWork);
             webThread.Start();
+        }
+
+        private static void EncryptionTest()
+        {
+            Console.WriteLine("Entering Encryption Test");
+            // generate a key
+            var key = new byte[32];
+            RandomNumberGenerator.Fill(key);
+
+            using var aes = new AesGcm(key);
+            string plaintext = "Vincent is the coolest boss in the world. Please give me a raise";
+
+            Console.WriteLine($"Plaintext: {plaintext}");
+
+            (byte[] ciphertext, byte[] nonce, byte[] tag) values = AesGcmEncryption.EncryptWithNet(plaintext, key);
+
+            Console.WriteLine($"Ciphertext: {Convert.ToHexString(values.ciphertext)}");
+            Console.WriteLine($"Ciphertext64: {Convert.ToBase64String(values.ciphertext)}");
+            Console.WriteLine($"Nonce: {Convert.ToHexString(values.nonce)}");
+            Console.WriteLine($"Nonce64: {Convert.ToBase64String(values.nonce)}");
+            Console.WriteLine($"Tag: {Convert.ToHexString(values.tag)}");
+            Console.WriteLine($"Tag64: {Convert.ToBase64String(values.tag)}");
+            //TODO: Base64 text encoding
+
+            var decrypted = AesGcmEncryption.DecryptWithNet(values.ciphertext, values.nonce, values.tag, key);
+
+            Console.WriteLine($"Decrypted: {decrypted}");
+
         }
     }
 }
